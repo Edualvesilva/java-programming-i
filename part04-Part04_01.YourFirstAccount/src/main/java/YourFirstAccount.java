df4dfb90fc2fe573df0913cb3b2dde13940f2cba
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-05-15 20:42:21 -0300

    solved until 04-09

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,15 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+
+        Account myAccount = new Account("Edu", 100);
+        myAccount.deposit(20);
+       // System.out.println(myAccount.balance());
+
+        System.out.println(myAccount);
+
+    }
+}
