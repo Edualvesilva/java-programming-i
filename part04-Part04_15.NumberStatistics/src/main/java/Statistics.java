4d0e1e42d8c308a58e5732760017a73316669c90
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-05-16 15:50:46 -0300

    solved untill 04-14

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,36 @@
 
 public class Statistics {
+
+    private int count;
+    private int sum;
+
+    public Statistics() {
+        int numberCount = 0;
+        this.count = numberCount;
+    }
+
+    public void addNumber(int number) {
+        this.count++;
+        this.sum += number;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public int sum() {
+
+        return this.sum;
+    }
+
+    public double average() {
+        double avg = (double) this.sum / this.count;
+
+        if (this.sum == 0) {
+            return 0;
+        }
+        return avg;
+
+    }
+
 }
