de8d6160403cca9bb5da4df1c890c1edbfd79798
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-05-15 21:08:25 -0300

    solved till 04-11

---
+++ part04-Part04_10.Song/src/main/java/Song.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Song {
+
+    private String songName;
+    private int songLength;
+
+    public Song(String name, int length) {
+        this.songName = name;
+        this.songLength = length;
+    }
+
+    public String name() {
+
+        return this.songName;
+    }
+
+    public int length() {
+        return this.songLength;
+    }
+
+}
