742a75976aa098c3a678251cb24126f42ec5f9fe
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-05-20 21:46:28 -0300

    solved until 04-20

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -1,12 +1,48 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        ArrayList<Books> book = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Title:");
+            String title = scanner.nextLine();
+
+            if (title.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Pages:");
+            int pages = Integer.valueOf(scanner.nextLine());
+
+            System.out.println("Publication year:");
+            int year = Integer.valueOf(scanner.nextLine());
+
+            
+            book.add(new Books(title,pages,year));
+        }
+
+        System.out.println("");
+        System.out.println("What information will be printed?");
+        String answer = scanner.nextLine();
+
+        for (Books eachBook : book) {
+            if (answer.equals("everything")) {
+                System.out.println(eachBook);
+            }
+
+            if (answer.equals("name")) {
+                System.out.println(eachBook.getName());
+            }
+
+        }
+
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
     }
 }
