435f2f9ff564c4a5cb27345c672a3f4f2e489424
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-05-20 22:30:09 -0300

    04-22

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,20 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
 
+        while (true) {
+            String text = scanner.nextLine();
+
+            if (text.equals("end")) {
+                break;
+            }
+
+//            int number = Integer.valueOf(text);
+            count++;
+
+        }
+
+        System.out.println(count);
     }
 }
