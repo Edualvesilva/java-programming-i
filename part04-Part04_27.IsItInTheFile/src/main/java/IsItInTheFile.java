e5a6c448730640e470c1189817e453073f86eba3
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-05-21 16:50:29 -0300

    till 04-29 solved

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -13,5 +13,26 @@ public class IsItInTheFile {
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
 
+        try ( Scanner scan = new Scanner(Paths.get(file))) {
+            boolean found = false;
+            while (scan.hasNextLine()) {
+                String name = scan.nextLine();
+                if (name.contains(searchedFor)) {
+                    found = true;
+                    System.out.println("Found!");
+
+                    break;
+                }
+
+            }
+
+            if (found == false) {
+                System.out.println("Not found.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Reading the file " + e + " failed.");
+        }
+
     }
 }
--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -7,13 +7,28 @@ public class NumbersFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("File? ");
+        System.out.println("File? ");
         String file = scanner.nextLine();
-        System.out.print("Lower bound? ");
+        System.out.println("Lower bound? ");
         int lowerBound = Integer.valueOf(scanner.nextLine());
-        System.out.print("Upper bound? ");
+
+        System.out.println("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        try ( Scanner scan = new Scanner(Paths.get(file))) {
+            int count = 0;
+            while (scan.hasNextLine()) {
+                int numberFile = Integer.valueOf(scan.nextLine());
+
+                if (numberFile >= lowerBound && numberFile <= upperBound) {
+                    count++;
+                }
+
+            }
+            System.out.println("Numbers: " + count);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
 }
