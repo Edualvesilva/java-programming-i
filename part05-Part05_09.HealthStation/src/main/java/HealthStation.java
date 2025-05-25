00c27f7b154c70506dccd1ad5c417b8df951374e
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-05-24 22:04:27 -0300

    solved until 05-10

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,23 @@
 
 public class HealthStation {
 
+   
+    private int weighings;
 
     public int weigh(Person person) {
+        this.weighings++;
+        return person.getWeight();
+       
         // return the weight of the person passed as the parameter
-        return -1;
+        
     }
 
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    public int weighings() {
+
+        return this.weighings;
+    }
 }
