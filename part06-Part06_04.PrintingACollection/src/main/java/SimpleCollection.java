1dc0986bbe2eee213d5544aa205082f0c8ff86ec
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-05-30 21:17:24 -0300

    solved untill part 06-06

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,31 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
-    
+
+    public String toString() {
+        ArrayList<String> total = this.getElements();
+
+        String elementName = "";
+        for (String names : total) {
+
+            if (elements.size() == 1) {
+                elementName = names;
+            } else {
+                elementName = elementName + names + "\n";
+            }
+        }
+
+        String printOut = "The collection " + this.name + " has " + this.elements.size() + " elements:" + "\n" + elementName;
+
+        if (elements.isEmpty()) {
+            return "The collection " + this.name + " is empty.";
+        }
+
+        if (elements.size() == 1) {
+            return "The collection " + this.name + " has " + this.elements.size() + " element:" + "\n" + elementName;
+        }
+
+        return printOut;
+    }
+
 }
